bb8974e6855218b728ab72a98bcd18c620cc8bfb
Jennifer Cristina <cristin4.jennifer@gmail.com>
2024-10-09 22:12:35 -0300

    feat: cria ciclo de vida para componentes

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, DoCheck {
+  title = 'app-lista-de-compras';
+  public listaDeCompra!: Array<Item>;
+
+  public itemParaSerEditado!: Item;
+
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+
+    console.log(this.listaDeCompra);
+
+  }
+
+  ngDoCheck(): void {
+    this.listaService.atualizarLocalStorage();
+  }
+
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
+
+  deletarItem(id: number) {
+    const index = this.listaDeCompra.findIndex((item) => item.id === id);
+
+    this.listaDeCompra.splice(index, 1);
+  }
+
+  limparLista() {
+    this.listaDeCompra = [];
+  }
+}
